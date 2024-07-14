064da26ab35736bf83c36507a1368e1d7244b0b3
Winnie <tzuhsuan1999@gmail.com>
2024-07-14 16:52:43 +0800

    new upload

---
+++ all.js
@@ -0,0 +1,1300 @@
+// 113年度會員名單(截至112年11月9日共138人)：
+// 會員名單
+
+// 中山團3人：
+// 呂文達、曾國雄、林崇榮。
+// 松山團6人：
+// 紀秀月、陸雨洲、簡銘俊、蔡美純、王國松、楊龍山。
+// 天母團3人：
+// 葉鴻儀、陳郁棻、梁柏禧。
+// 台晨團8人：
+// 朱明中、江御、李協興、邱珮瑜、張伯鋒、陳酉軍、黃燦煌、吳孟珍。
+// 台夜團24人：
+// 王建立、王思屏、古昆泰、吳明信、邱承堯、范乃元、張雅惠、張瑀庭、梁雅茹、郭俊毅、陳德生、舒繼光、楊振斌、蔡宗豪、賴永輝、賴智傑、戴聖展、鍾少鈞、林建彰、黃國明、文素珍、宋涵涵、陳正雄、陳建閔。
+// 信義團27人：
+// 古奇哲、吳昌澤、吳鎮安、李宏祐、周志柔、林育良、林欣芳、林漢哲、林福德、洪玉玲、洪鴻祥、紀和成、胡文秋、范慈娟、梁佑阡、陳金楣、陳奕成、陳姿蓉、陳淑君、葉博淵、鄧永宏、鍾方晨、魏麗惠、卓羿君、黃成效、許雅薇、蘇彥邦。
+// 新莊團7人：
+// 吳秋紅、姚志杰、陳惠琴、焦丕康、黃雅苓、吳京諭、曾靖雅。
+// 基隆團4人：
+// 徐薇雅、張宇麟、劉謙任、劉豐銘。
+// 桃園團10人：
+// 李仲周、林桃秀、翁明汝、劉怡梅、蔡森淇、謝奇洲、杜泓逸、范嬿宜、古素琴、張雪珍。
+// 新竹團2人：
+// 詹前鵬、李冠霆。
+// 台中黎明團20人：
+// 王永俊、王郁翔、王晨琪、呂冠霖、李紹慈、林季寬、姚威旭、施伯青、胡志新、張瑋真、陳亞娜、陳致銘、陳雁棻、賴淑蘭、賴鴻麒、董佑銓、劉素貞、林柏成、陳嘉峰、李穎超。
+// 台中松竹團4人：
+// 朱恆、温祐靖、吳佩涵、劉彥良。
+// 台南團4人：
+// 洪媛玲、林兆忠、林家鴻、陳柏毓。
+// 高雄團16人：
+// 朱正國、吳青芳、呂千毓、李孟憲、李冠儀、李庭旭、林育慈、林萃蘋、林敬章、童湄芸、辜進心、黃英俞、魏敏如、吳啟東、孫崇庭、楊宏輝。
+
+
+//陪跑志工培訓名單
+// 姓名	梯次	報名跑團
+// 洪良其	53	信義團
+// 羅玄微	52	中山團
+// 張諾帆	52	中山團
+// 王彥智	52	中山團
+// 李芝蓉	52	中山團
+// 林冠華	52	台夜團
+// 陳亭融	52	台夜團
+// 吳宗鴻	52	台夜團
+// 馮佩佩	52	台夜團
+// 張瀞文	52	台夜團
+// 陳思婷	52	台夜團
+// 黃韋凱	52	台晨團
+// 郭姉寧	52	台晨團
+// 洪瓊芬	52	台晨團
+// 徐志南	52	台晨團
+// 胡鎮璽	52	松山團
+// 蔡培林	52	松山團
+// 吳姿璇	52	信義團
+// 王世宏	52	信義團
+// 林于萍	52	信義團
+// 黃禹綸	52	信義團
+// 黃世全	52	信義團
+// 吳書婷	52	信義團
+// 陳怡汝	52	信義團
+// 吳柏成	52	桃園團
+// 李玉如	52	桃園團
+// 羅國日	52	桃園團
+// 郭美玲	52	桃園團
+// 何宜娟	52	桃園團
+// 樊紹廣	52	桃園團
+// 羅楷頡	52	桃園團
+// 魏楊德	52	基隆團
+// 黃文彬	52	新莊團
+// 丁子涵	52	新莊團
+// 梁佑慈	52	新莊團
+// 賴玟穎	52	新莊團
+// 賴孟傑	52	新莊團
+// 余惠萍	52	新莊團
+// 張妙如	52	新莊團
+// 趙學揚	52	新莊團
+// 周有軒	51	中山團
+// 信燕興	51	松山團
+// 連柄界	51	基隆團
+// 何宗翰	51	桃園團
+// 王毅君	51	桃園團
+// 李嬋娟	51	桃園團
+// 張明鈞	51	桃園團
+// 葉春到	51	新莊團
+// 洪翠芬	51	新莊團
+// 張景嫙	51	新莊團
+// 王信富	51	天母團
+// 黃珮芝	51	天母團
+// 廖雅郁	51	信義團
+// 許美惠	51	信義團
+// 王金玉	51	信義團
+// 洪慧諭	51	信義團
+// 陳少榛	51	信義團
+// 林家進	51	信義團
+// 楊志偉	51	台夜團
+// 張馨元	51	台夜團
+// 林均怡	51	台夜團
+// 陳志魁	51	台夜團
+// 張書瑋	51	台夜團
+// 馬應娥	51	台晨團
+// 楊曜華	51	台晨團
+// 陳坤山	50	桃園團
+// 羅玉叡	50	松山團
+// 潘柳青	49	新竹團
+// 林芷綺	49	新竹團
+// 張力文	49	新竹團
+// 黃啓宸	49	天母團
+// 邱世榮	49	天母團
+// 曾昭欽	49	新莊團
+// 李錦華	49	新莊團
+// 沈麗芬	49	台晨團
+// 陳曉玲	49	台晨團
+// 吳義良	49	台晨團
+// 謝祥興	49	台晨團
+// 古欣怡	49	台晨團
+// 梁銘心 49	信義團
+// 鍾宜庭	49	信義團
+// 徐惠敏	49	信義團
+// 林亞廷	49	信義團
+// 許麗花	49	信義團
+// 郭瑞慶	49	信義團
+// 沈佩玲	49	信義團
+// 廖思為	49	信義團
+// 許麗嬌	49	信義團
+// 張瑞蘭	49	信義團
+// 徐慧齡	49	信義團
+// 詹閔惠	49	信義團
+// 江靜慧	49	台夜團
+// 許勝文	49	台夜團
+// 陳姿吟	49	台夜團
+// 謝政憲	49	台夜團
+// 廖建鈜	49	台夜團
+// 李珮華	49	台夜團
+// 林珈羽	49	台夜團
+// 張耘馨	49	基隆團
+// 張恩誠	48	新竹團
+// 黃伯元	48	松山團
+// 李文琳	48	松山團
+// 蕭珮頤	48	松山團
+// 出秉弘	48	新莊團
+// 陳清峰	48	新莊團
+// 葉益宗	48	新莊團
+// 張玉香	48	新莊團
+// 李昭青	48	台晨團
+// 陳啟明	48	台晨團
+// 李昀	 48	 台晨團
+// 陳文斌	48	台晨團
+// 黃政剛	48	信義團
+// 邰思瑀	48	信義團
+// 施依伶	48	信義團
+// 陳國銘	48	信義團
+// 林主珣	48	信義團
+// 曾宇薇	48	信義團
+// 王淑慧	48	信義團
+// 宋郁明	48	信義團
+// 梁如馨	48	信義團
+// 蔡佩芸	48	信義團
+// 張雅惠	48	信義團
+// 柳承志	48	台夜團
+// 王聖鳳	48	台夜團
+// 劉真瑩	48	台夜團
+// 彭碧筠	48	台夜團
+// 李健萁	48	台夜團
+// 陳世昌	48	台夜團
+// 何佳穎	47	天母團
+// 詹正義	47	桃園團
+// 劉鏵傛	46	新竹團
+// 藍依婷	46	桃園團
+// ​李芷筠 46	 桃園團
+// 謝文彬	46	桃園團
+// 江相芝	46	桃園團
+// 崔德明	46	桃園團
+// 劉會華	46	基隆團
+// 吳麗娟	46	松山團
+// 温婉吟	46	松山團
+// 葉明彥	46	新莊團
+// 陳品如	46	新莊團
+// 吳青玲	46	新莊團
+// 李志豪	46	台晨團
+// ​劉騏瑋 46	 台晨團
+// 楊惠茹	46	台晨團
+// 張書睿	46	台晨團
+// 邵仕剛	46	台夜團
+// ​邵子耘 46	 台夜團
+// 羅少塏	46	台夜團
+// 鍾惠婷	46	台夜團
+// ​劉信泰 46	 台夜團
+// 翁毓涓	46	信義團
+// ​林品樂 46	 信義團
+
+
+let data = [{
+    type: "視障朋友",
+    nickName: "昌澤",
+    name: "吳昌澤"
+  },
+  {
+    type: "視障朋友",
+    nickName: "秀蘭",
+    name: "黃秀蘭"
+  },
+  {
+    type: "視障朋友",
+    nickName: "子曄",
+    name: "黃子曄"
+  },
+  {
+    type: "視障朋友",
+    nickName: "宏祐",
+    name: "李宏祐"
+  },
+  {
+    type: "視障朋友",
+    nickName: "文秋",
+    name: "胡文秋"
+  },
+  {
+    type: "視障朋友",
+    nickName: "阿達",
+    name: "呂文達"
+  },
+  {
+    type: "視障朋友",
+    nickName: "褔德",
+    name: "林褔德"
+  },
+  {
+    type: "視障朋友",
+    nickName: "玉玲",
+    name: "洪玉玲"
+  },
+  {
+    type: "視障朋友",
+    nickName: "羿君",
+    name: "卓羿君"
+  },
+  {
+    type: "視障朋友",
+    nickName: "佑阡",
+    name: "梁佑阡"
+  },
+  {
+    type: "視障朋友",
+    nickName: "小葉",
+    name: "葉博淵"
+  },
+  {
+    type: "視障朋友",
+    nickName: "姿蓉",
+    name: "陳姿蓉"
+  },
+  {
+    type: "視障朋友",
+    nickName: "雅茹",
+    name: "張雅茹"
+  },
+  {
+    type: "視障朋友",
+    nickName: "淑君",
+    name: "陳淑君"
+  },
+  {
+    type: "視障朋友",
+    nickName: "娟娟",
+    name: "范慈娟"
+  },
+  {
+    type: "視障朋友",
+    nickName: "安仔",
+    name: "吳鎮安"
+  },
+  {
+    type: "視障朋友",
+    nickName: "雅薇",
+    name: "許雅薇"
+  },
+  {
+    type: "視障朋友",
+    nickName: "志杰",
+    name: "姚志杰"
+  },
+  {
+    type: "視障朋友",
+    nickName: "佩芳",
+    name: "劉佩芳"
+  },
+
+  {
+    type: "視障朋友",
+    nickName: "楣楣",
+    name: "陳金楣"
+  },
+  {
+    type: "視障朋友",
+    nickName: "鴻祥",
+    name: "洪鴻祥"
+  },
+  {
+    type: "視障朋友",
+    nickName: "欣芳",
+    name: "林欣芳"
+  },
+  {
+    type: "視障朋友",
+    nickName: "金希",
+    name: "金希"
+  },
+  {
+    type: "視障朋友",
+    nickName: "特爾",
+    name: "紀和成"
+  },
+  {
+    type: "視障朋友",
+    nickName: "思屏",
+    name: "王思屏"
+  },
+  {
+    type: "視障朋友",
+    nickName: "淑怡",
+    name: "楊淑怡"
+  },
+  {
+    type: "視障朋友",
+    nickName: "福德",
+    name: "林福德"
+  },
+  {
+    type: "視障朋友",
+    nickName: "童大哥",
+    name: "童元良"
+  },
+  {
+    type: "視障朋友",
+    nickName: "漢哲",
+    name: "林漢哲"
+  },
+  {
+    type: "視障朋友",
+    nickName: "曉宇",
+    name: "賴永輝"
+  },
+  {
+    type: "視障朋友",
+    nickName: "育良",
+    name: "林育良"
+  },
+  {
+    type: "視障朋友",
+    nickName: "靜如",
+    name: "蔡靜如"
+  },
+  {
+    type: "視障朋友",
+    nickName: "火龍",
+    name: "黃火龍"
+  },
+  {
+    type: "視障朋友",
+    nickName: "涵涵",
+    name: "宋涵涵"
+  },
+  {
+    type: "視障朋友",
+    nickName: "阿校",
+    name: "黃成校"
+  },
+  {
+    type: "視障朋友",
+    nickName: "承堯",
+    name: "邱承堯"
+  },
+  {
+    type: "視障朋友",
+    nickName: "大牛",
+    name: "古奇哲"
+  },
+  {
+    type: "視障朋友",
+    nickName: "彥邦",
+    name: "蘇彥邦"
+  },
+  {
+    type: "視障朋友",
+    nickName: "奕成",
+    name: "陳奕成"
+  },
+  {
+    type: "視障朋友",
+    nickName: "謙任",
+    name: "劉謙任"
+  },
+  {
+    type: "視障朋友",
+    nickName: "佳瑩",
+    name: "盧佳瑩"
+  },
+  {
+    type: "視障朋友",
+    nickName: "芝芝",
+    name: "芝芝"
+  },
+  {
+    type: "視障朋友",
+    nickName: "永宏",
+    name: "鄧永宏"
+  },
+  {
+    type: "視障朋友",
+    nickName: "志柔",
+    name: "周志柔"
+  },
+  {
+    type: "視障朋友",
+    nickName: "靖雅",
+    name: "曾靖雅"
+  },
+  {
+    type: "視障朋友",
+    nickName: "宗裕",
+    name: "陳宗裕"
+  },
+  {
+    type: "視障朋友",
+    nickName: "敝Jacky",
+    name: "姚志杰"
+  },
+  {
+    type: "視障朋友",
+    nickName: "育慧",
+    name: "陳育慧"
+  },
+  {
+    type: "視障朋友",
+    nickName: "柏禧",
+    name: "梁柏禧"
+  },
+
+  {
+    type: "陪跑員",
+    nickName: "欣蓓",
+    name: "林欣蓓"
+  },
+  {
+    type: "陪跑員",
+    nickName: "米雅",
+    name: "黃卉沄"
+  },
+  {
+    type: "陪跑員",
+    nickName: "莎校長",
+    name: "陳美雲"
+  },
+  {
+    type: "陪跑員",
+    nickName: "美惠",
+    name: "許美惠"
+  },
+  {
+    type: "陪跑員",
+    nickName: "姿璇",
+    name: "吳姿璇"
+  },
+  {
+    type: "陪跑員",
+    nickName: "于萍",
+    name: "林于萍"
+  },
+  {
+    type: "陪跑員",
+    nickName: "靖雅",
+    name: "林靖雅"
+  },
+  {
+    type: "陪跑員",
+    nickName: "青玲",
+    name: "吳青玲"
+  },
+  {
+    type: "陪跑員",
+    nickName: "毓青",
+    name: "詹毓青"
+  },
+  {
+    type: "陪跑員",
+    nickName: "明俐",
+    name: "李明俐"
+  },
+  {
+    type: "陪跑員",
+    nickName: "淑玲",
+    name: "吳淑玲"
+  },
+  {
+    type: "陪跑員",
+    nickName: "曉芬",
+    name: "詹曉芬"
+  },
+  {
+    type: "陪跑員",
+    nickName: "豆媽",
+    name: "吳嘉惠"
+  },
+  {
+    type: "陪跑員",
+    nickName: "豆爸",
+    name: "劉治群"
+  },
+  {
+    type: "陪跑員",
+    nickName: "國基",
+    name: "凌國基"
+  },
+  {
+    type: "陪跑員",
+    nickName: "小蓉包",
+    name: "周佳蓉"
+  },
+  {
+    type: "陪跑員",
+    nickName: "翠琴",
+    name: "莊翠琴"
+  },
+  {
+    type: "陪跑員",
+    nickName: "旻諭",
+    name: "鍾旻諭"
+  },
+  {
+    type: "陪跑員",
+    nickName: "承宜",
+    name: "林承宜"
+  },
+  {
+    type: "陪跑員",
+    nickName: "麗花",
+    name: "許麗花"
+  },
+  {
+    type: "陪跑員",
+    nickName: "國銘",
+    name: "陳國銘"
+  },
+  {
+    type: "陪跑員",
+    nickName: "小向",
+    name: "向益寬"
+  },
+  {
+    type: "陪跑員",
+    nickName: "雅郁",
+    name: "廖雅郁"
+  },
+  {
+    type: "陪跑員",
+    nickName: "世全",
+    name: "黃世全"
+  },
+  {
+    type: "陪跑員",
+    nickName: "禹綸",
+    name: "黃禹綸"
+  },
+  {
+    type: "陪跑員",
+    nickName: "金玉",
+    name: "王金玉"
+  },
+  {
+    type: "陪跑員",
+    nickName: "怡汝",
+    name: "陳怡汝"
+  },
+  {
+    type: "陪跑員",
+    nickName: "書婷",
+    name: "吳書婷"
+  },
+  {
+    type: "陪跑員",
+    nickName: "思婷",
+    name: "陳思婷"
+  },
+  {
+    type: "陪跑員",
+    nickName: "阿其",
+    name: "洪良其"
+  },
+  {
+    type: "陪跑員",
+    nickName: "鴻如",
+    name: "簡鴻如"
+  },
+  {
+    type: "陪跑員",
+    nickName: "阿寶",
+    name: "李桂芳"
+  },
+  {
+    type: "陪跑員",
+    nickName: "禹禎",
+    name: "刁禹禎"
+  },
+  {
+    type: "陪跑員",
+    nickName: "郁棻",
+    name: "王郁棻"
+  },
+  {
+    type: "陪跑員",
+    nickName: "慧娟",
+    name: "李慧娟"
+  },
+  {
+    type: "陪跑員",
+    nickName: "志盈",
+    name: "周志盈"
+  },
+  {
+    type: "陪跑員",
+    nickName: "宗慶",
+    name: "林宗慶"
+  },
+  {
+    type: "陪跑員",
+    nickName: "紅豆",
+    name: "邱莞晴"
+  },
+  {
+    type: "陪跑員",
+    nickName: "溪哥",
+    name: "邱錦溪"
+  },
+  {
+    type: "陪跑員",
+    nickName: "瑞紋",
+    name: "洪瑞紋"
+  },
+  {
+    type: "陪跑員",
+    nickName: "豫芬",
+    name: "秦豫芬"
+  },
+  {
+    type: "陪跑員",
+    nickName: "淑芳",
+    name: "梁淑芳"
+  },
+  {
+    type: "陪跑員",
+    nickName: "宣伊",
+    name: "陳宣伊"
+  },
+  {
+    type: "陪跑員",
+    nickName: "大宗",
+    name: "惲大宗"
+  },
+  {
+    type: "陪跑員",
+    nickName: "敏華",
+    name: "曾敏華"
+  },
+  {
+    type: "陪跑員",
+    nickName: "政偉",
+    name: "黃政偉"
+  },
+  {
+    type: "陪跑員",
+    nickName: "敏玲",
+    name: "廖敏玲"
+  },
+  {
+    type: "陪跑員",
+    nickName: "祥宇",
+    name: "榮祥宇"
+  },
+  {
+    type: "陪跑員",
+    nickName: "阿路",
+    name: "鄭文儒"
+  },
+  {
+    type: "陪跑員",
+    nickName: "宗堯",
+    name: "鄭宗堯"
+  },
+  {
+    type: "陪跑員",
+    nickName: "宏宗",
+    name: "謝宏宗"
+  },
+  {
+    type: "陪跑員",
+    nickName: "靄玲",
+    name: "謝靄玲"
+  },
+  {
+    type: "陪跑員",
+    nickName: "巧琪",
+    name: "闕巧琪"
+  },
+  {
+    type: "陪跑員",
+    nickName: "筱雯",
+    name: "嚴筱雯"
+  },
+  {
+    type: "陪跑員",
+    nickName: "家寬",
+    name: "林家寬"
+  },
+  {
+    type: "陪跑員",
+    nickName: "怡如",
+    name: "陳怡如"
+  },
+  {
+    type: "陪跑員",
+    nickName: "惠美",
+    name: "陳惠美"
+  },
+  {
+    type: "陪跑員",
+    nickName: "玠穎",
+    name: "陳玠穎"
+  },
+  {
+    type: "陪跑員",
+    nickName: "品樂",
+    name: "林品樂"
+  },
+  {
+    type: "陪跑員",
+    nickName: "毓涓",
+    name: "翁毓涓"
+  },
+  {
+    type: "陪跑員",
+    nickName: "小艾",
+    name: "黃瑞玲"
+  },
+  {
+    type: "陪跑員",
+    nickName: "莉亞",
+    name: "林莉亞"
+  },
+  {
+    type: "陪跑員",
+    nickName: "嘉蓁",
+    name: "胡嘉蓁"
+  },
+  {
+    type: "陪跑員",
+    nickName: "笑笑",
+    name: "張立琦"
+  },
+  {
+    type: "陪跑員",
+    nickName: "雅民",
+    name: "王雅民"
+  },
+  {
+    type: "陪跑員",
+    nickName: "君達",
+    name: "魏君達"
+  },
+  {
+    type: "陪跑員",
+    nickName: "桂蘭",
+    name: "邱桂蘭"
+  },
+  {
+    type: "陪跑員",
+    nickName: "閻哥",
+    name: "閻正君"
+  },
+  {
+    type: "陪跑員",
+    nickName: "政剛",
+    name: "黃政剛"
+  },
+  {
+    type: "陪跑員",
+    nickName: "思瑀",
+    name: "邰思瑀"
+  },
+  {
+    type: "陪跑員",
+    nickName: "主珣",
+    name: "林主珣"
+  },
+  {
+    type: "陪跑員",
+    nickName: "宇薇",
+    name: "曾宇薇"
+  },
+  {
+    type: "陪跑員",
+    nickName: "郁明",
+    name: "宋郁明"
+  },
+  {
+    type: "陪跑員",
+    nickName: "依伶",
+    name: "施依伶"
+  },
+  {
+    type: "陪跑員",
+    nickName: "淑慧",
+    name: "王淑慧"
+  },
+  {
+    type: "陪跑員",
+    nickName: "佩芸",
+    name: "蔡佩芸"
+  },
+  {
+    type: "陪跑員",
+    nickName: "雅惠",
+    name: "張雅惠"
+  },
+  {
+    type: "陪跑員",
+    nickName: "銘心",
+    name: "梁銘心"
+  },
+  {
+    type: "陪跑員",
+    nickName: "宜庭",
+    name: "鍾宜庭"
+  },
+  {
+    type: "陪跑員",
+    nickName: "惠敏",
+    name: "徐惠敏"
+  },
+  {
+    type: "陪跑員",
+    nickName: "亞廷",
+    name: "林亞廷"
+  },
+  {
+    type: "陪跑員",
+    nickName: "瑞慶",
+    name: "郭瑞慶"
+  },
+  {
+    type: "陪跑員",
+    nickName: "佩玲",
+    name: "沈佩玲"
+  },
+  {
+    type: "陪跑員",
+    nickName: "思為",
+    name: "廖思為"
+  },
+  {
+    type: "陪跑員",
+    nickName: "麗嬌",
+    name: "許麗嬌"
+  },
+  {
+    type: "陪跑員",
+    nickName: "瑞蘭",
+    name: "張瑞蘭"
+  },
+  {
+    type: "陪跑員",
+    nickName: "慧齡",
+    name: "徐慧齡"
+  },
+  {
+    type: "陪跑員",
+    nickName: "閔惠",
+    name: "詹閔惠"
+  },
+  {
+    type: "陪跑員",
+    nickName: "家進",
+    name: "林家進"
+  },
+  {
+    type: "陪跑員",
+    nickName: "慧諭",
+    name: "洪慧諭"
+  },
+  {
+    type: "陪跑員",
+    nickName: "少榛",
+    name: "陳少榛"
+  },
+  {
+    type: "陪跑員",
+    nickName: "枃怡",
+    name: "林枃怡"
+  },
+  {
+    type: "陪跑員",
+    nickName: "建鈞",
+    name: "林建鈞"
+  },
+  {
+    type: "陪跑員",
+    nickName: "哲凰",
+    name: "廖哲凰"
+  },
+  {
+    type: "陪跑員",
+    nickName: "冠伃",
+    name: "陳冠伃"
+  },
+  {
+    type: "陪跑員",
+    nickName: "菊歡",
+    name: "梁菊歡"
+  },
+  {
+    type: "陪跑員",
+    nickName: "文彥",
+    name: "臧文彥"
+  },
+  {
+    type: "陪跑員",
+    nickName: "冠華",
+    name: "林冠華"
+  },
+  {
+    type: "陪跑員",
+    nickName: "世宏",
+    name: "王世宏"
+  },
+  {
+    type: "陪跑員",
+    nickName: "彩瑱",
+    name: "許彩瑱"
+  },
+  {
+    type: "陪跑員",
+    nickName: "士豪",
+    name: "黃士豪"
+  },
+  {
+    type: "陪跑員",
+    nickName: "逸萍",
+    name: "江逸萍"
+  },
+  {
+    type: "陪跑員",
+    nickName: "文林",
+    name: "邱文林"
+  },
+  {
+    type: "陪跑員",
+    nickName: "偉倩",
+    name: "高偉倩"
+  },
+  {
+    type: "陪跑員",
+    nickName: "玉香",
+    name: "張玉香"
+  },
+  {
+    type: "陪跑員",
+    nickName: "漢強",
+    name: "劉漢強"
+  },
+  {
+    type: "陪跑員",
+    nickName: "常宇",
+    name: "王常宇"
+  },
+  {
+    type: "陪跑員",
+    nickName: "雅惠",
+    name: "羅雅惠"
+  },
+  {
+    type: "陪跑員",
+    nickName: "婉吟",
+    name: "温婉吟"
+  },
+  {
+    type: "陪跑員",
+    nickName: "家宏",
+    name: "李家宏"
+  },
+  {
+    type: "陪跑員",
+    nickName: "月娥",
+    name: "侯月娥"
+  },
+  {
+    type: "陪跑員",
+    nickName: "淑君",
+    name: "盧淑君"
+  },
+  {
+    type: "陪跑員",
+    nickName: "品萱",
+    name: "吳品萱"
+  },
+  {
+    type: "陪跑員",
+    nickName: "心慧",
+    name: "陳心慧"
+  },
+  {
+    type: "陪跑員",
+    nickName: "宥芯",
+    name: "陳宥芯"
+  },
+  {
+    type: "陪跑員",
+    nickName: "怡青",
+    name: "李怡青"
+  },
+  {
+    type: "陪跑員",
+    nickName: "是旭",
+    name: "陳是旭"
+  },
+  {
+    type: "陪跑員",
+    nickName: "宇玫",
+    name: "莊宇玫"
+  },
+  {
+    type: "陪跑員",
+    nickName: "Choetsow",
+    name: "Choetsow"
+  },
+  {
+    type: "陪跑員",
+    nickName: "鈺淇",
+    name: "徐鈺淇"
+  },
+  {
+    type: "陪跑員",
+    nickName: "Emma",
+    name: "蘇豫萱"
+  },
+  {
+    type: "陪跑員",
+    nickName: "詠淳",
+    name: "李詠淳"
+  },
+  {
+    type: "陪跑員",
+    nickName: "Noah",
+    name: "Noah"
+  },
+  {
+    type: "陪跑員",
+    nickName: "麗君",
+    name: "顏麗君"
+  },
+  {
+    type: "陪跑員",
+    nickName: "郭婕",
+    name: "郭婕"
+  },
+  {
+    type: "陪跑員",
+    nickName: "嘉嬥",
+    name: "林嘉嬥"
+  },
+  {
+    type: "陪跑員",
+    nickName: "辰娟",
+    name: "簡辰娟"
+  },
+  {
+    type: "陪跑員",
+    nickName: "珮甄",
+    name: "李珮甄"
+  },
+  {
+    type: "陪跑員",
+    nickName: "韋志",
+    name: "陳韋志"
+  },
+  {
+    type: "陪跑員",
+    nickName: "郁雯",
+    name: "郁雯"
+  },
+  {
+    type: "陪跑員",
+    nickName: "凱茹",
+    name: "詹凱茹"
+  },
+  {
+    type: "陪跑員",
+    nickName: "Austin",
+    name: "Austin"
+  },
+  {
+    type: "陪跑員",
+    nickName: "杰倫",
+    name: "謝杰倫"
+  },
+  {
+    type: "陪跑員",
+    nickName: "阿隆嫂",
+    name: "吳正隆"
+  },
+  {
+    type: "陪跑員",
+    nickName: "Anya",
+    name: "Anya"
+  },
+  {
+    type: "陪跑員",
+    nickName: "渝婷",
+    name: "曾渝婷"
+  },
+  {
+    type: "陪跑員",
+    nickName: "嘉輝",
+    name: "王嘉輝"
+  },
+  {
+    type: "陪跑員",
+    nickName: "佩君",
+    name: "林佩君"
+  },
+  {
+    type: "陪跑員",
+    nickName: "倩冷",
+    name: "陳倩冷"
+  },
+  {
+    type: "陪跑員",
+    nickName: "孟婷",
+    name: "洪孟婷"
+  },
+  {
+    type: "陪跑員",
+    nickName: "佩茜",
+    name: "張佩茜"
+  },
+  {
+    type: "陪跑員",
+    nickName: "怡蘋",
+    name: "洪怡蘋"
+  },
+  {
+    type: "陪跑員",
+    nickName: "義彬",
+    name: "姜義彬"
+  },
+  {
+    type: "陪跑員",
+    nickName: "Luck",
+    name: "Luck"
+  },
+  {
+    type: "陪跑員",
+    nickName: "郁璇",
+    name: "黃郁璇"
+  },
+  {
+    type: "陪跑員",
+    nickName: "小李",
+    name: "李虹徵"
+  },
+  {
+    type: "陪跑員",
+    nickName: "友儀",
+    name: "盧友儀"
+  },
+  {
+    type: "陪跑員",
+    nickName: "麗君",
+    name: "颜麗君"
+  },
+  {
+    type: "陪跑員",
+    nickName: "韻庭",
+    name: "邱韻庭"
+  },
+  {
+    type: "陪跑員",
+    nickName: "國龍",
+    name: "陳國龍"
+  },
+  {
+    type: "陪跑員",
+    nickName: "Austin",
+    name: "Austin"
+  },
+  {
+    type: "陪跑員",
+    nickName: "Stacy",
+    name: "孫新閔"
+  },
+  {
+    type: "陪跑員",
+    nickName: "書郁",
+    name: "王書郁"
+  },
+  {
+    type: "陪跑員",
+    nickName: "苡絃",
+    name: "張苡絃"
+  },
+  {
+    type: "陪跑員",
+    nickName: "旻民",
+    name: "蔡旻民"
+  },
+  {
+    type: "陪跑員",
+    nickName: "怡蘋",
+    name: "洪怡蘋"
+  },
+  {
+    type: "陪跑員",
+    nickName: "庭瑋",
+    name: "張庭瑋"
+  },
+  {
+    type: "陪跑員",
+    nickName: "珈羽",
+    name: "林珈羽"
+  },
+  {
+    type: "陪跑員",
+    nickName: "宗鴻",
+    name: "吳宗鴻"
+  },
+];
+// 叮咚～今天是seven eleven 日，所以我們把小小七搬來國館啦！越早接龍補給越豐富🐷
+
+// 日期: 7月11日星期四
+// 時間: 晚上7:30     
+// 地點: 國父紀念館捷運站4號出口(如需引導請出聲) 
+
+// 視障朋友：阿達、姿蓉、小葉、福德、謙任、子曄、佩芳、小地瓜、宏祐、文秋、秀蘭、承堯、佑阡、育慧、雅茹、玉玲、昌澤、宗裕、淑怡、楣楣、欣芳、柏禧、安仔（23）
+
+// 陪跑員：金玉、美惠、冠華、宇玫、紅豆、國基、世全、庭瑋、林于萍、思瑀、雅郁、怡汝、書婷、豆爸、豆媽、瑞蘭、文彥、菊歡、思婷、鈺淇、豫芬、阿其、靖雅、益寬（24）
+
+// 新視友: 黃靜湄、林君達
+
+// 新陪跑員：
+
+// 顧包包：常宇、敝Jacky
+// 補給小幫手：小向、姿璇
+let str = ``;
+
+const finalText = document.querySelector(".finalText");
+const textarea = document.querySelector(".textarea");
+const covertBtn = document.querySelector(".covertBtn");
+const clearBtn = document.querySelector(".clearBtn");
+const copyBtn = document.querySelector(".copyBtn");
+
+
+//全名轉換
+function changeName() {
+  data.forEach(function (item, index) {
+    let regex = new RegExp(item.nickName, "g");
+    str = str.replace(regex, item.name);
+  });
+}
+
+covertBtn.addEventListener("click", function (e) {
+  str = textarea.value;
+  changeName(str);
+  finalText.innerHTML = str.replace(/\n/g, '<br>'); // 保持換行符號
+});
+
+//清空欄位
+function clearText() {
+  textarea.value = "";
+};
+clearBtn.addEventListener("click", function (e) {
+  clearText();
+});
+
+
+//複製轉換後接龍文
+function copyText() {
+  //建立 range 物件
+  const range = document.createRange();
+  range.selectNode(finalText);
+  //取得 selection 物件
+  const selection = window.getSelection();
+  selection.removeAllRanges(); //確保獲取的是新的，且也不會出現反白的狀況
+  selection.addRange(range);
+  document.execCommand("copy"); //執行瀏覽器複製命令
+  alert("已複製文字！")
+  selection.removeAllRanges(); //確保獲取的是新的，且也不會出現反白的狀況
+  finalText.innerHTML = "";
+  textarea.value = "";
+};
+
+copyBtn.addEventListener("click", function (e) {
+  copyText();
+});
